abd975e9f1fc1681e568a049647be11bc30fdacb
DncHsyn <huseyin.dinc1996@gmail.com>
2025-06-01 16:46:30 +0300

    Update PlayerMovement.cs

--- MyPlatformer/Assets/Scripts/PlayerMovement.cs
+++ MyPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -2,45 +2,110 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private Rigidbody2D rb;
-    
-    [Header("Jumping")]
-    [SerializeField] private float jumpForce = 5f;
+    public Rigidbody2D rb;
+    bool isFacingRight = true;
 
-    [Header("Ground Check")]
-    public Transform groundCheck;
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    public int maxJumps = 2;
+    int jumpsRemaining;
+
+    [Header("GroundCheck")] 
+    public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.2f);
     public LayerMask groundLayer;
-    float horizontalMove;
+
+    [Header("Gravity")]
+    public float baseGravity = 2f;
+    public float maxFallSpeed = 18f;
+    public float fallMultiplier = 2f;
+
+    [Header("WallCheck")] 
+    public Transform wallCheckPos;
+    public Vector2 wallCheckSize = new Vector2(0.5f, 0.2f);
+    public LayerMask wallLayer;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(horizontalMove * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        GroundCheck();
+        Gravity();
+    }
 
+    private void Gravity()
+    {
+        if(rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallMultiplier; //baseGravity normal yerçekimi katsayısıdır. fallMultiplier ile çarpılarak düşüş sırasında daha güçlü bir çekim kuvveti uygulanır.
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed)); //Düşüş hızı sınırlamak için Mathf.Max kullanılır.
+        }
+        else
+        {
+            rb.gravityScale = baseGravity; //normal yerçekimi kuvveti nesne yukarı çıkarken uygulanır.
+            
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        horizontalMove = context.ReadValue<Vector2>().x;
+        horizontalMovement = context.ReadValue<Vector2>().x;
     }
-
+    
     public void Jump(InputAction.CallbackContext context)
+{
+    if(jumpsRemaining > 0)
     {
-        if (context.performed)
+        if(context.performed)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        } else if (context.canceled)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+                jumpsRemaining--;
+            } 
+            else if(context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+                jumpsRemaining--;
+            }
         }
     }
 
+
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps;
+        }
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+    
+    
+    
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
-        Gizmos.DrawCube(groundCheck.position, groundCheckSize);
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(wallCheckPos.position, wallCheckSize);
     }
 }
